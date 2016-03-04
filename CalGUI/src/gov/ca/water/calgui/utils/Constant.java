7e34c2a7718385b001b70c6f918a8e5eb76c087e
Willy <willy.wang@touchpointsinc.com>
2016-03-04 14:21:33 -0800

    Initial version.

---
+++ CalGUI/src/gov/ca/water/calgui/utils/Constant.java
@@ -0,0 +1,9 @@
+package gov.ca.water.calgui.utils;
+
+public class Constant {
+	public static String TO_ADDRESS = "xxxx@gmail.com";
+	public static String FROM_ADDRESS = "yyyy@gmail.com";
+	public static String SUBJECT = "Test";
+	public static String USER_NAME = "xxxx";
+	public static String PASSWORD = "password";
+}
